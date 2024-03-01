c9671c5d34662bb55f99db6644fd16b24611d2d6
TianshengC <123282349+TianshengC@users.noreply.github.com>
2024-03-01 23:53:07 +0000

    update structure

--- server/utils/findEarlistScheduledDate.js
+++ server/utils/findEarlistScheduledDate.js
@@ -1,13 +1,13 @@
- function findEarliestScheduledDate(reviewItem) {
+function findEarliestScheduledDate(reviewItem) {
 
-    const scheduledSessions = reviewItem.reviewSessions.filter(session => session.status === "Scheduled");
-    
-    // If there are no scheduled sessions, return a distant future date as a placeholder
-    if (scheduledSessions.length === 0) return '9999-12-31';
-    
-    
-    scheduledSessions.sort((a, b) => new Date(a.scheduled_date) - new Date(b.scheduled_date));
-    return scheduledSessions[0].scheduled_date;
-  }
+  const scheduledSessions = reviewItem.reviewSessions.filter(session => session.status === "Scheduled");
 
-  module.exports = findEarliestScheduledDate;
+  // If there are no scheduled sessions, return a distant future date as a placeholder
+  if (scheduledSessions.length === 0) return '9999-12-31';
+
+
+  scheduledSessions.sort((a, b) => new Date(a.scheduled_date) - new Date(b.scheduled_date));
+  return scheduledSessions[0].scheduled_date;
+}
+
+module.exports = findEarliestScheduledDate;
